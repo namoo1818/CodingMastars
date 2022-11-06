b7b7d410c321b4b8a090fa7aafa8366cc99a53d4
minji <3dlalswl3@naver.com>
2022-11-07 00:50:36 +0900

    달고나 풀이

--- 4310 달고나.py
+++ 4310 달고나.py
@@ -1,4 +1,4 @@
-# bfs문제
+# 첫시도(틀림)
 
 n, m = map(int, input().split())
 dalgona = []
@@ -22,7 +22,36 @@ for i in range(n):
                 count += 1
 print(count)
 
+# DFS 풀이
 
+from collections import deque
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(input()))
+
+def dfs(x,y):
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+    if graph[x][y] == '0':
+        graph[x][y] = '1'
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+count = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            count += 1
+print(count)
+
+
+# BFS 풀이
 
 
 """
