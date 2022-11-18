3cdfedbdd538d34f40074c33fe2239f837ca920a
minji <3dlalswl3@naver.com>
2022-11-18 10:07:13 +0900

    dfs

--- 4305 관광지.py
+++ 4305 관광지.py
@@ -3,12 +3,16 @@
 # BFS
 from collections import deque
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end=' ')
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
     
             
 
@@ -27,5 +31,5 @@ for i in range(n):
 print(graph)
 
 visited = [False] * 9
-dfs(graph, 1, visited)
+bfs(graph, 1, visited)
 
